4394ff87b4c75eb44ff48888e36284ccf2feb68c
rogerdavidvera <rovera@vassar.edu>
2018-11-14 16:31:56 -0500

    custom links for each product_sheet products to .csv

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :products, only: [:index, :update, :show, :create]
+
       resources :product_sheets, only: [:index, :update, :show, :create]
+
+      get 'product_sheets/download(/:id)', to: 'product_sheets#download'
+
     end
   end
 
